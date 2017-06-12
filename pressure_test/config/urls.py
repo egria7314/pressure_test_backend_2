8c05738e4e7d946a741d0242a1a82f073fcbb7a9
ethan.kuo <ethan.kuo@vivotek.com>
2017-06-12 14:32:30 +0800

    update for add default db and api

---
+++ pressure_test/config/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+# from pressure_test.camera_log import views
+from rest_framework.urlpatterns import format_suffix_patterns
+from config import views
+
+# from website import views
+
+
+
+urlpatterns = [
+    url(r'^init_default', views.init_default_setting),
+    url(r'^default/$', views.return_default_setting)
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
