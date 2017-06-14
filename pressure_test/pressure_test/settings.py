5e5c13f4fd3b2cb4900d9e3576e1a67c4d9c861e
ethan.kuo <ethan.kuo@vivotek.com>
2017-06-14 10:50:48 +0800

    update for cosheaders

--- pressure_test/pressure_test/settings.py
+++ pressure_test/pressure_test/settings.py
@@ -40,17 +40,28 @@ INSTALLED_APPS = [
     'rest_framework',
     'camera_log',
     'config',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+# CORS_ORIGIN_WHITELIST = (
+#     'google.com',
+#     'hostname.example.com',
+#     'localhost:8000',
+#     '127.0.0.1:9000'
+# )
 
 ROOT_URLCONF = 'pressure_test.urls'
 
