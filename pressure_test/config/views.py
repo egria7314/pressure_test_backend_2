2e98df7a21386b8dfb88ba8c8bfa9b760ae26ee7
ethan.kuo <ethan.kuo@vivotek.com>
2017-06-21 14:48:27 +0800

    update for modify utc to local time

--- pressure_test/config/views.py
+++ pressure_test/config/views.py
@@ -11,6 +11,7 @@ from config.models import DefaultSetting
 from libs.nas_storage import NasStorage
 from rest_framework import status
 from rest_framework.decorators import api_view
+from django.utils.timezone import localtime
 from libs.telnet_module import URI
 import re, collections
 
@@ -154,7 +155,9 @@ def return_project_setting(requests, pk=None):
         return_json['projectName'] = return_json.pop('project_name')
         return_json['cameraIp'] = return_json.pop('ip')
         return_json['startTime'] = return_json.pop('start_time')
+        return_json['startTime'] = localtime(return_json['startTime'])
         return_json['endTime'] = return_json.pop('end_time')
+        return_json['endTime'] = localtime(return_json['end_time'])
 
     else:
         querry_set = ProjectSetting.objects.all().values("id", "path", "project_name", "start_time", "log", "delay", "end_time",
@@ -165,7 +168,9 @@ def return_project_setting(requests, pk=None):
             item_json['projectName'] = item_json.pop('project_name')
             item_json['cameraIp'] = item_json.pop('ip')
             item_json['startTime'] = item_json.pop('start_time')
+            item_json['startTime'] = localtime(item_json['startTime'])
             item_json['endTime'] = item_json.pop('end_time')
+            item_json['endTime'] = localtime(item_json['endTime'])
     return Response(return_json)
 
 @api_view(['GET'])
