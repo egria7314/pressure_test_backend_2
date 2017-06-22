46e875aa71d2fb958adb6556518f672e267bba91
carlos <carlos.hu@vivotek.com>
2017-06-22 10:59:55 +0800

    medium & high load

--- pressure_test/recording_continous/tasks.py
+++ pressure_test/recording_continous/tasks.py
@@ -10,6 +10,7 @@ import os
 import datetime, time
 from config.models import ProjectSetting
 from libs.nas_storage import NasStorage
+from libs.vast_storage import VastStorage
 
 from recording_continous.models import RecordingContinuty
 from recording_continous.models import RecordingFile
@@ -17,26 +18,31 @@ from recording_continous.models import Config
 from recording_continous.video_continous import VideoContinous
 
 def arrange_periodic_task(project_id, start_time, end_time):
-    ns = NasStorage()
     obj = ProjectSetting.objects.get(project_name=project_id)
     remote_path = obj.path
     remote_username = obj.path_username
     remote_password = obj.path_password
     sudo_password = ""
     prefix = obj.prefix_name
-
-    clips = ns.get_video_nas(remote_username, remote_password, sudo_password, remote_path, prefix, start_time, end_time)
+    pressure_test_video_type = obj.type
+    if pressure_test_video_type == 'medium':
+        vast = VastStorage()
+        clips = vast.get_video_vast(remote_username, remote_password, sudo_password, remote_path, start_time, end_time)
+    elif pressure_test_video_type == 'high':
+        ns = NasStorage()
+        clips = ns.get_video_nas(remote_username, remote_password, sudo_password, remote_path, prefix, start_time, end_time)
 
     for file_path in sorted(clips):
-        push_detect_broken_image_tasks_to_queue(remote_username, remote_password, project_id, remote_path, file_path)
+        push_detect_broken_image_tasks_to_queue(remote_username, remote_password, project_id, remote_path, file_path, pressure_test_video_type)
         time.sleep(5)
 
     # for file_path in clips:
     #     push_detect_broken_image_tasks_to_queue.apply_async(args=[remote_username, remote_password, project_id, remote_path, file_path],queue='continous_test_camera{}'.format(str(project_id)))
     #     time.sleep(5)
 
-def push_detect_broken_image_tasks_to_queue(remote_username, remote_password, project_id, remote_path, file_path):
+def push_detect_broken_image_tasks_to_queue(remote_username, remote_password, project_id, remote_path, file_path, pressure_test_video_type):
     from libs.nas_storage import NasStorage
+    from libs.vast_storage import VastStorage
     from recording_continous.models import Config
     from recording_continous.video_continous import VideoContinous
     from recording_continous.models import RecordingFile
@@ -44,8 +50,17 @@ def push_detect_broken_image_tasks_to_queue(remote_username, remote_password, pr
 
     local_path = os.path.join("/mnt/", remote_path.replace('//', '').replace('/', '_'))
     clippath = os.path.join(local_path, file_path.lower()[len(remote_path) + 1:])
-    ns = NasStorage()
-    ns.mount_folder(remote_username, remote_password, remote_path, '', local_path)
+
+
+    if pressure_test_video_type == 'medium':
+        vast = VastStorage()
+        vast.mount_folder(remote_username, remote_password, remote_path, '', local_path)
+    elif pressure_test_video_type == 'high':
+        ns = NasStorage()
+        ns.mount_folder(remote_username, remote_password, remote_path, '', local_path)
+
+
+
     query = RecordingFile.objects.filter(project_id=project_id).order_by('-id')[0]
     file_path_before = query.path
 
@@ -62,7 +77,15 @@ def push_detect_broken_image_tasks_to_queue(remote_username, remote_password, pr
     print("PUSH QUEUE:", clippath)
     vc = VideoContinous(file_path_before, clippath)
     in_result = vc.continuity_in_recording_files()
-    between_result = vc.continuity_bwtween_recording_files()
+    query = RecordingFile.objects.filter(project_id=project_id)
+    if len(query)>0:
+        between_result = vc.continuity_bwtween_recording_files()
+    else :
+        between_result ={}
+        between_result["between_result"] = 'failed'
+        between_result["seconds"] = 'First video'
+
+
 
     RecordingContinuty.objects.create(
         project_id=project_id,
