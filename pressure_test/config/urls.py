dc693f883b4e68b17c3a0af7a66e228704c41535
ethan.kuo <ethan.kuo@vivotek.com>
2017-06-26 10:48:15 +0800

    update for add stop test function

--- pressure_test/config/urls.py
+++ pressure_test/config/urls.py
@@ -15,7 +15,8 @@ urlpatterns = [
     url(r'^projects/(?P<pk>[0-9]+)/$', views.return_project_setting),
     url(r'^projects/$', views.return_project_setting),
     url(r'^save_project_setting/$', views.post),
-    url(r'^daily-summary/(?P<pk>[0-9]+)/$', views.return_daily_summary)
+    url(r'^daily-summary/(?P<pk>[0-9]+)/$', views.return_daily_summary),
+    url(r'^stop-test/(?P<pk>[0-9]+)/$', views.stop_running_test)
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
