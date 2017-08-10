4ff857e51db68c6e32082012be92f2c02b6de18a
leohsu <leo.hsu@vivotek.com>
2017-08-10 15:04:18 +0800

    add url "project-status"

--- pressure_test/config/urls.py
+++ pressure_test/config/urls.py
@@ -16,7 +16,8 @@ urlpatterns = [
     url(r'^projects/$', views.return_project_setting),
     url(r'^save_project_setting/$', views.post),
     url(r'^daily-summary/(?P<pk>[0-9]+)/$', views.return_daily_summary),
-    url(r'^stop-test/(?P<pk>[0-9]+)/$', views.stop_running_test)
+    url(r'^stop-test/(?P<pk>[0-9]+)/$', views.stop_running_test),
+    url(r'^project-status/', views.project_counting)
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
